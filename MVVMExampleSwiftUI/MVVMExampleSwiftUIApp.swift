eb5b4bb0811fb4d5cb87f5d8c7bd195d3b413a7c
Clifford Sharp <csharpusa@gmail.com>
2023-03-21 12:45:48 -0500

    Initial Commit

---
+++ MVVMExampleSwiftUI/MVVMExampleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMExampleSwiftUIApp.swift
+//  MVVMExampleSwiftUI
+//
+//  Created by Cliff Sharp on 3/21/23.
+//
+
+import SwiftUI
+
+@main
+struct MVVMExampleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
